b37aaec5e7533d7590e4cd0e2a3e9884a5720d04
Naule Sousa <davenstall@gmail.com>
2022-12-20 22:48:14 -0300

    storing a variable amount of data with vectors

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,8 @@ fn main() {
         Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
         Visitor::new("fred", "Wow, who invited Fred?"),
     ];
-//    let mut allow_them_in = false;
 
+loop {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
 
@@ -46,12 +46,18 @@ let known_visitor = visitor_list
 
 match known_visitor {
     Some(visitor) => visitor.greet_visitor(),
-    None => println!("You are not on the visitor list. Please leave.")
+    None => {
+        if name.is_empty() {
+            break;
+        } else {
+            println!("{} is not on the visitor list.", name);
+            visitor_list.push(Visitor::new(&name, "New friend"));
+        }
+    }
 }
+break;
+}
+println!("The final list of visitors:");
+println!("{:#?}", visitor_list);
 
-//    if allow_them_in {
-//        println!("Welcome to the Treehouse, {}", name);
-//    } else {
-//        println!("Sorry, you aren't on the list.");
-//    }
 }
