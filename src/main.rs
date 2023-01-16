cde1ff972ae9a82e9a6048118001311ab58b684a
Naule Sousa <davenstall@gmail.com>
2023-01-15 22:13:28 -0300

    reformating and fix

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::stdin;
 #[derive(Debug)]
 enum VisitorAction {
     Accept,
-    AcceptWithNote { note: String},
+    AcceptWithNote { note: String },
     Refuse,
     Probation,
 }
@@ -12,7 +12,7 @@ struct Visitor {
     name: String,
     greeting: String,
     action: VisitorAction,
-    age: i8
+    age: i8,
 }
 
 impl Visitor {
@@ -21,12 +21,12 @@ impl Visitor {
             name: name.to_lowercase(),
             greeting: greeting.to_string(),
             action,
-            age
+            age,
         }
     }
-    
-fn greet_visitor(&self) {
-        math &self.action {
+
+    fn greet_visitor(&self) {
+        match &self.action {
             VisitorAction::Accept => println!("Welcome to the tree house, {}", self.name),
             VisitorAction::AcceptWithNote { note } => {
                 println!("Welcome to the treehouse, {}", self.name);
@@ -34,7 +34,7 @@ fn greet_visitor(&self) {
                 if self.age < 21 {
                     println!("Do not serve alcohol to {}", self.name)
                 }
-            },
+            }
             VisitorAction::Probation => println!("{} is now a probationary member", self.name),
             VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
         }
@@ -46,43 +46,44 @@ fn what_is_your_name() -> String {
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    your_name
-    .trim()
-    .to_lowercase()
-} 
+    your_name.trim().to_lowercase()
+}
 
 fn main() {
-    let my_action = VisitorAction::AcceptWithNote{ note: "Give them a taco"};
+    let my_action = VisitorAction::AcceptWithNote {
+        note: "Give them a taco",
+    };
     let mut visitor_list = vec![
         Visitor::new("Bert", VisitorAction::Accept, 45),
-        Visitor::new("steve", VisitorAction::AcceptWithNote{
-            note: String::from("Lactose-free milk is in the fridge")
-        }, 15),
+        Visitor::new(
+            "steve",
+            VisitorAction::AcceptWithNote {
+                note: String::from("Lactose-free milk is in the fridge"),
+            },
+            15,
+        ),
         Visitor::new("fred", VisitorAction::Refuse, 30),
     ];
 
-loop {
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
+    loop {
+        println!("Hello, what's your name?");
+        let name = what_is_your_name();
 
-let known_visitor = visitor_list
-    .iter()
-    .find(|visitor| visitor.name == name);
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
 
-match known_visitor {
-    Some(visitor) => visitor.greet_visitor(),
-    None => {
-        if name.is_empty() {
-            break;
-        } else {
-            println!("{} is not on the visitor list.", name);
-            visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
+                }
+            }
         }
+        break;
     }
-}
-break;
-}
-println!("The final list of visitors:");
-println!("{:#?}", visitor_list);
-
+    println!("The final list of visitors:");
+    println!("{:#?}", visitor_list);
 }
